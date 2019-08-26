35f1a2f633a27b5c02f25b2893c412b6230f15ad
RodrigoGM97 <rodrigogm971@gmail.com>
2019-08-26 06:42:56 -0500

    Added pyramid

--- A2_3d_Figures/3d_figures.js
+++ A2_3d_Figures/3d_figures.js
@@ -73,6 +73,122 @@ function initGL(canvas)
 
 // TO DO: Create the functions for each of the figures.
 
+//Pyramid
+function createPyramid(gl, translation, rotationAxis){
+    var vertexBuffer;
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var verts = [
+       //Face 1
+       0.0, -1.0, -1.275,
+       1.214, -1.0, -0.394,
+        0.0,  1.0,  0.0,
+  
+       // Face 2
+       1.214, -1.0, -0.394,
+       0.75, -1.0, 1.032,
+        0.0,  1.0, 0.0,
+  
+       // Face 3
+       0.75, -1.0, 1.032,
+       -0.75, -1.0, 1.032,
+       0.0, 1.0, 0.0,
+  
+       // Pentagon
+        0.0, -1.0, -1.275,
+        1.214, -1.0, -0.394,
+        0.75, -1.0, 1.032,
+  
+        0.0, -1.0, -1.275,
+        0.75, -1.0, 1.032,
+        -0.75, -1.0, 1.032,
+  
+        0.0, -1.0, -1.275,
+        -0.75, -1.0, 1.032,
+        -1.214, -1.0,-0.394,
+  
+       // Face 4
+       -0.75, -1.0, 1.032,
+       -1.214, -1.0,-0.394,
+       0.0, 1.0, 0.0,
+  
+       // Face 5
+       -1.214, -1.0,-0.394,
+       0.0, -1.0, -1.275,
+       0.0, 1.0, 0.0
+       ];
+  
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+  
+    // Color data
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    var faceColors = [
+      [1.0, 0.0, 0.0], // Face 1
+      [0.0, 1.0, 0.0], // Face 2
+      [0.0, 0.0, 1.0], // Face 3
+      [1.0, 1.0, 0.0], // Pentagon
+      [0.0, 1.0, 1.0], // Pentagon
+      [0.0, 0.0, 0.0], // Pentagon
+      [1.0, 1.0, 1.0], // Face 4
+      [1.0, 0.5, 1.0]  // Face 5
+    ];
+  
+    // Assigning the vertex color information
+    var vertexColors = [];
+    for (const color of faceColors)
+    {
+        for (var j=0; j < 3; j++)
+            vertexColors = vertexColors.concat(color);
+    }
+  
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+  
+    // Create the triangle index
+    var pyramidIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndexBuffer);
+    var pyramidIndices = [
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+        12, 13, 14,
+        15 ,16, 17,
+        18, 19, 20,
+        21, 22, 23
+    ];
+  
+    // gl.ELEMENT_ARRAY_BUFFER: Buffer used for element indices.
+    // Uint16Array: Array of 16-bit unsigned integers.
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidIndices), gl.STATIC_DRAW);
+  
+    var pyramid = {
+            buffer:vertexBuffer, colorBuffer:colorBuffer, indices:pyramidIndexBuffer,
+            vertSize:3, nVerts:verts.length, colorSize:3, nColors: faceColors.length, nIndices:pyramidIndices.length,
+            primtype:gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime : Date.now()};
+  
+    mat4.translate(pyramid.modelViewMatrix, pyramid.modelViewMatrix, translation);
+  
+    pyramid.update = function()
+    {
+        var now = Date.now();
+        var deltat = now - this.currentTime;
+        this.currentTime = now;
+        var fract = deltat / duration;
+        var angle = Math.PI * 2 * fract;
+  
+        // Rotates a mat4 by the given angle
+        // mat4 out the receiving matrix
+        // mat4 a the matrix to rotate
+        // Number rad the angle to rotate the matrix by
+        // vec3 axis the axis to rotate around
+        mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+    };
+  
+    return pyramid;
+
+}
+
 function createShader(gl, str, type)
 {
     var shader;
@@ -125,6 +241,38 @@ function initShader(gl)
 function draw(gl, objs) 
 {
     // TO DO: Fill in the draw function to draw all the objects
+        // clear the background (with black)
+        gl.clearColor(0.1, 0.1, 0.1, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.clear(gl.COLOR_BUFFER_BIT  | gl.DEPTH_BUFFER_BIT);
+    
+        // set the shader to use
+        gl.useProgram(shaderProgram);
+    
+        for(i = 0; i<objs.length; i++)
+        {
+            obj = objs[i];
+            // connect up the shader parameters: vertex position, color and projection/model matrices
+            // set up the buffers
+            gl.bindBuffer(gl.ARRAY_BUFFER, obj.buffer);
+            gl.vertexAttribPointer(shaderVertexPositionAttribute, obj.vertSize, gl.FLOAT, false, 0, 0);
+    
+            gl.bindBuffer(gl.ARRAY_BUFFER, obj.colorBuffer);
+            gl.vertexAttribPointer(shaderVertexColorAttribute, obj.colorSize, gl.FLOAT, false, 0, 0);
+            
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indices);
+    
+            gl.uniformMatrix4fv(shaderProjectionMatrixUniform, false, projectionMatrix);
+            gl.uniformMatrix4fv(shaderModelViewMatrixUniform, false, obj.modelViewMatrix);
+    
+            // Draw the object's primitives using indexed buffer information.
+            // void gl.drawElements(mode, count, type, offset);
+            // mode: A GLenum specifying the type primitive to render.
+            // count: A GLsizei specifying the number of elements to be rendered.
+            // type: A GLenum specifying the type of the values in the element array buffer.
+            // offset: A GLintptr specifying an offset in the element array buffer.
+            gl.drawElements(obj.primtype, obj.nIndices, gl.UNSIGNED_SHORT, 0);
+        }
 }
 
 function run(gl, objs) 
