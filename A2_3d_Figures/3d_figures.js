14c58bba38557560d8de19c38869fc32b6f5be23
RodrigoGM97 <rodrigogm971@gmail.com>
2019-08-28 09:07:18 -0500

    Finished all figures and movements

--- A2_3d_Figures/3d_figures.js
+++ A2_3d_Figures/3d_figures.js
@@ -120,7 +120,7 @@ function createPyramid(gl, translation, rotationAxis){
   
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
   
-    // Color data
+    // Assigning color to each of the faces
     var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     var faceColors = [
@@ -188,6 +188,334 @@ function createPyramid(gl, translation, rotationAxis){
     return pyramid;
 
 }
+
+//Octahedron
+function createOctahedron(gl, translation, rotationAxis,canvasHeight)
+{
+  var move_Y = 0;
+  var going_up = true;
+  var vertexBuffer;
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  var verts = [
+     // Face 1
+     0.7071, 0.0, 0.0,
+     0.0, 0.0,  0.7071,
+     0.0, 0.7071, 0.0,
+
+     // Face 2
+     0.7071, 0.0, 0.0,
+     0.0, -0.7071, 0.0,
+     0.0, 0.0,  0.7071,
+
+     // Face 3
+     0.0, 0.0,  0.7071,
+     -0.7071, 0.0, 0.0,
+     0.0, 0.7071, 0.0,
+
+     // Face 4
+     0.0, 0.0,  0.7071,
+     0.0, -0.7071, 0.0,
+     -0.7071, 0.0, 0.0,
+
+     // Face 5
+     -0.7071, 0.0, 0.0,
+     0.0, 0.0,  -0.7071,
+     0.0, 0.7071, 0.0,
+
+     //Face 6
+     -0.7071, 0.0, 0.0,
+     0.0, -0.7071, 0.0,
+     0.0, 0.0,  -0.7071,
+
+     //Face 7
+     0.0, 0.0,  -0.7071,
+     0.7071, 0.0, 0.0,
+     0.0, 0.7071, 0.0,
+
+     //Face 8
+     0.0, 0.0,  -0.7071,
+     0.0, -0.7071, 0.0,
+     0.7071, 0.0, 0.0
+     ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+  // Assigning color to each of the faces
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  var faceColors = [
+      [1.0, 0.0, 0.0], // Face 1
+      [0.0, 1.0, 0.0], // Face 2
+      [0.0, 0.0, 1.0], // Face 3
+      [1.0, 1.0, 0.0], // Face 4
+      [1.0, 0.0, 1.0], // Face 5
+      [0.0, 0.5, 0.5], // Face 6
+      [1.0, 1.0, 1.0], // Face 7
+      [0.7, 0.0, 0.0]  // Face 8
+  ];
+
+  // Assigning the vertex color information
+  var vertexColors = [];
+  for (const color of faceColors)
+  {
+      for (var j=0; j < 3; j++)
+          vertexColors = vertexColors.concat(color);
+  }
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+  // Create the triangle index
+  var octahedronIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, octahedronIndexBuffer);
+  var octahedronIndices = [
+      0, 1, 2,
+      3, 4, 5,
+      6, 7, 8,
+      9, 10, 11,
+      12, 13, 14,
+      15,16, 17,
+      18, 19, 20,
+      21, 22, 23
+  ];
+
+  // gl.ELEMENT_ARRAY_BUFFER: Buffer used for element indices.
+  // Uint16Array: Array of 16-bit unsigned integers.
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(octahedronIndices), gl.STATIC_DRAW);
+
+  var octahedron = {
+          buffer:vertexBuffer, colorBuffer:colorBuffer, indices:octahedronIndexBuffer,
+          vertSize:3, nVerts:verts.length, colorSize:3, nColors: faceColors.length, nIndices:octahedronIndices.length,
+          primtype:gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime : Date.now()};
+
+  mat4.translate(octahedron.modelViewMatrix, octahedron.modelViewMatrix, translation);
+  octahedron.update = function()
+  {
+      var now = Date.now();
+      var deltat = now - this.currentTime;
+      this.currentTime = now;
+      var fract = deltat / duration;
+      var angle = Math.PI * 2 * fract;
+
+      // Rotates a mat4 by the given angle
+      // mat4 out the receiving matrix
+      // mat4 a the matrix to rotate
+      // Number rad the angle to rotate the matrix by
+      // vec3 axis the axis to rotate around
+      mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+      //Set vertical movement
+      if(going_up){
+        move_Y += 1;
+        mat4.translate(octahedron.modelViewMatrix, octahedron.modelViewMatrix, [0,0.01,0]);
+        //Check the vertical movement limit
+        if(move_Y >= canvasHeight/1.5){
+          going_up = false; //Now time to go down
+        }
+      }
+      else{
+        move_Y -= 1;
+        mat4.translate(octahedron.modelViewMatrix, octahedron.modelViewMatrix, [0,-0.01,0]);
+        //Check the vertical movement limit
+        if(move_Y < -canvasHeight/2){
+          going_up = true;//Now time to go up
+        }
+      }
+    };
+
+  return octahedron;
+}
+
+//Scutoid
+function createScutoid(gl, translation, rotationAxis)
+{
+  var vertexBuffer;
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  var verts = [
+    // Base Pentagon
+    0.0, -1.0, -1.275,
+    1.214, -1.0, -0.394,
+    0.75, -1.0, 1.032,
+
+    0.0, -1.0, -1.275,
+    0.75, -1.0, 1.032,
+    -0.75, -1.0, 1.032,
+
+    0.0, -1.0, -1.275,
+    -0.75, -1.0, 1.032,
+    -1.214, -1.0,-0.394,
+
+    //Face 1
+    -0.75, -1.0, 1.032,
+    -1.214, -1.0,-0.394,
+    -0.75, 1.0, 1.11,
+
+    -1.214, -1.0,-0.394,
+    -1.275, 1.0, 0.0,
+    -0.75, 1.0, 1.11,
+
+     //Face 2
+     0.75, -1.0, 1.032,
+     -0.75, -1.0, 1.032,
+     0.75, 1.0, 1.11,
+
+     -0.75, -1.0, 1.032,
+     -0.75, 1.0, 1.11,
+     0.75, 1.0, 1.11,
+
+    // Face 3
+    1.214, -1.0, -0.394,
+    0.75, -1.0, 1.032,
+    1.275, 1.0, 0.0,
+
+    0.75, -1.0, 1.032,
+    0.75, 1.0, 1.11,
+    1.275, 1.0, 0.0,
+
+    //Base Hexagon
+    0.75, 1.0, -1.11,
+    1.275, 1.0, 0.0,
+    0.75, 1.0, 1.11,
+
+    0.75, 1.0, -1.11,
+    0.75, 1.0, 1.11,
+    -0.75, 1.0, -1.11,
+
+    -0.75, 1.0, -1.11,
+    0.75, 1.0, 1.11,
+    -0.75, 1.0, 1.11,
+
+    -0.75, 1.0, -1.11,
+    -1.275, 1.0, 0.0,
+    -0.75, 1.0, 1.11,
+
+    //Face 4
+    1.275, 1.0, 0.0,
+    0.75, 1.0, -1.11,
+    0.0, -1.0, -1.275,
+
+    0.0, -1.0, -1.275,
+    1.214, -1.0, -0.394,
+    1.275, 1.0, 0.0,
+
+    0.75, 1.0, -1.11,
+    0.0, -0.074, -1.275,
+    0.0, -1.0, -1.275,
+
+    //Triangle
+    0.75, 1.0, -1.11,
+    -0.75, 1.0, -1.11,
+    0.0, -0.074, -1.275,
+
+    //Face 5
+    -1.275, 1.0, 0.0,
+    -1.214, -1.0,-0.394,
+    0.0, -1.0, -1.275,
+
+    -1.275, 1.0, 0.0,
+    0.0, -1.0, -1.275,
+    -0.75, 1.0, -1.11,
+
+    -0.75, 1.0, -1.11,
+    0.0, -1.0, -1.275,
+    0.0, -0.074, -1.275
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+  // Color data
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  var faceColors = [
+      [1.0, 0.0, 0.0], 
+      [1.0, 0.0, 0.0], 
+      [1.0, 0.0, 0.0], 
+      [0.0, 1.0, 0.0], 
+      [0.0, 1.0, 0.0], 
+      [0.0, 0.0, 1.0], 
+      [0.0, 0.0, 1.0], 
+      [0.5, 1.0, 0.9], 
+      [0.5, 1.0, 0.9], 
+      [0.5, 1.0, 0.0], 
+      [0.5, 1.0, 0.0], 
+      [0.5, 1.0, 0.0], 
+      [0.5, 1.0, 0.0], 
+      [1.0, 1.0, 0.0], 
+      [1.0, 1.0, 0.0], 
+      [1.0, 1.0, 0.0], 
+      [1.0, 0.3, 1.0], 
+      [0.8, 1.0, 1.0], 
+      [0.8, 1.0, 1.0], 
+      [0.8, 1.0, 1.0] 
+  ];
+
+  // Assigning the vertex color information
+  var vertexColors = [];
+
+  for (const color of faceColors)
+  {
+      for (var j=0; j < 3; j++)
+          vertexColors = vertexColors.concat(color);
+  }
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+  // Create the triangle index
+  var scutoidIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, scutoidIndexBuffer);
+  var scutoidIndices = [
+      0, 1, 2, 
+      3, 4, 5, 
+      6, 7, 8, 
+      9, 10, 11,
+      12, 13, 14,
+      15, 16, 17,
+      18, 19, 20,
+      21, 22, 23,
+      24, 25, 26,
+      27,28,29,
+      30,31,32,
+      33,34,35,
+      36,37,38,
+      39,40,41,
+      42,43,44,
+      45,46,47,
+      48,49,50,
+      51,52,53,
+      54,55,56,
+      57,58,59
+  ];
+
+  // gl.ELEMENT_ARRAY_BUFFER: Buffer used for element indices.
+  // Uint16Array: Array of 16-bit unsigned integers.
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(scutoidIndices), gl.STATIC_DRAW);
+
+  var scutoid = {
+          buffer:vertexBuffer, colorBuffer:colorBuffer, indices:scutoidIndexBuffer,
+          vertSize:3, nVerts:verts.length, colorSize:3, nColors: faceColors.length, nIndices:scutoidIndices.length,
+          primtype:gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime : Date.now()};
+
+  mat4.translate(scutoid.modelViewMatrix, scutoid.modelViewMatrix, translation);
+
+  scutoid.update = function()
+  {
+      var now = Date.now();
+      var deltat = now - this.currentTime;
+      this.currentTime = now;
+      var fract = deltat / duration;
+      var angle = Math.PI * 2 * fract;
+
+      // Rotates a mat4 by the given angle
+      // mat4 out the receiving matrix
+      // mat4 a the matrix to rotate
+      // Number rad the angle to rotate the matrix by
+      // vec3 axis the axis to rotate around
+      mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+  };
+
+  return scutoid;
+}
+
 
 function createShader(gl, str, type)
 {
